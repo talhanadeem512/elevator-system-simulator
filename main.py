0cbbee36bd4ebe4f47c00fed91c0cd2b25f6ec58
Talha Nadeem <talhanadeem1054@gmail.com>
2026-08-21 23:01:00 +0200

    Add break condition and movement phase loop

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ def run():
     # 1. Show the building before asking for input
     elevator.display_building()
 
-    while true:
+    while True:
         print("Enter the floor you want to go.")
         print("Press enter button or type 'done' if everyone is inside and elevator is ready to move")
-        dest = input()
+        dest = input("> ")
         if dest.isdigit():
             dest = int(dest)
+            elevator.add_request(dest)
 
-        elif dest == '\n' or dest.lower() == 'done':
-            elevator.move()
+        elif dest == "" or dest.lower() == 'done':
+            print("Closing the doors. Preparing to move...")
+            break
 
-    # 2. Prompt user for destination
-    dest = int(input("\nEnter destination floor (0-10): "))
-    elevator.add_request(dest)
+        else:
+            print("Enter a valid floor number.")
 
+        print(f"\nUp Queue: {elevator.up_queue}")
+        print(f"Down Queue: {elevator.down_queue}")
+
+    print("\n--- ELEVATOR IS MOVING ---")
+    while len(elevator.up_queue) > 0 or len(elevator.down_queue) 0:
+        elevator.move()
 if __name__ == "__main__":
     run()
